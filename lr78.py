4cdff1f83e47e6c58810ee5e9865dc555767d694
Gromy4 <mr.gromy4@gmail.com>
2021-04-14 18:40:08 +0300

    Commit(Zapolnil questions(adress and mail))

--- lr78.py
+++ lr78.py
@@ -1,9 +1,10 @@
 import pdb
 
-questions = {'How r u ?' ,'How u doin?','Why r u runnin?'}
 
-pdb.set_trace()
 mail = "ADRESS"
 text = 'QUEST'
-questions[mail] = text
+questions = {mail, text}
+pdb.set_trace()
+
+#questions[mail] = text
 
